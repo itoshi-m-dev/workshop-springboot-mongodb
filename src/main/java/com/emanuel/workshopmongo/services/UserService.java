a9ab9f7603bc15f188f1245754d57b2b4a8aa152
itoshi-m-dev <emanuelkalison@gmail.com>
2025-10-21 16:53:49 -0300

    getting object by id

--- src/main/java/com/emanuel/workshopmongo/services/UserService.java
+++ src/main/java/com/emanuel/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.emanuel.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emanuel.workshopmongo.domain.User;
 import com.emanuel.workshopmongo.repositories.UserRepository;
+import com.emanuel.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,12 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+		
 	}
 
 }
